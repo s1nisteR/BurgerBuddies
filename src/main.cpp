291d710f33d93de220a6283b549d3b346b475401
s1nisteR <ankon1441@gmail.com>
2022-08-30 01:25:06 +0600

    A few tests with std::thread

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <chrono>
+#include <thread>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+constexpr int RACK_SIZE = 10;               //size of the rack
+constexpr int COOKING_TIME_MS = 1000;       //cooking time of each burger, in milliseconds
+
+int rack = 0;
+
+[[noreturn]] void cook()
+{
+    while(true)
+    {
+        //check if rack is full
+        if(rack >= RACK_SIZE)
+            continue; //if rack full, do nothing
+        else
+        {
+            //otherwise, we COOK
+            std::this_thread::sleep_for(std::chrono::milliseconds(COOKING_TIME_MS)); //cooking each burger takes 1 second
+            rack++; //increment rack
+        }
+        std::cout << "Rack: " << rack << std::endl;
+    }
+}
+
+
+int main()
+{
+    std::thread cook_thread(cook);
+    cook_thread.join();
+    std::cout << "Burger Buddies Problem" << std::endl;
     return 0;
 }
