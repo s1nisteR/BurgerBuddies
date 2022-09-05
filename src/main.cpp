cf6bf1817ae70df2343ba9d2781b0a7ccf4a2fd9
s1nisteR <ankon1441@gmail.com>
2022-09-06 01:28:23 +0600

    Updated project

--- src/main.cpp
+++ src/main.cpp
@@ -2,67 +2,135 @@
 #include <chrono>
 #include <thread>
 #include <semaphore>
+#include <syncstream>
+#include <fstream>
+#include <inicpp.h>
 
 
-constexpr int RACK_SIZE = 10;               //size of the rack
-constexpr int COOKING_TIME_MS = 500;       //cooking time of each burger, in milliseconds
+int RACK_SIZE = 0;               //size of the rack
+int COOKING_TIME_MS = 0;       //cooking time of each burger, in milliseconds
+int NUMBER_OF_COOKS = 0;          //number of cooks
+int NUMBER_OF_CASHIERS = 0;       //number of cashiers
+int NUMBER_OF_CUSTOMERS = 0;     //number of customers
 
 int rack = 0;
 
 std::binary_semaphore rackMutex(1);
-std::counting_semaphore cashierSemaphore(0);
-std::counting_semaphore cookSemaphore(RACK_SIZE);
+std::counting_semaphore fullCount(0);
+std::counting_semaphore emptyCount(RACK_SIZE);
+std::binary_semaphore customerMutex(1);
+std::counting_semaphore customerSem(0);
+std::counting_semaphore burger(0);
 
-[[noreturn]] void cook()
+[[noreturn]] void cook(int id)
 {
     while(true)
     {
-        cookSemaphore.acquire(); //decrement cook sem
+        emptyCount.acquire(); //decrement cook sem
 
-        std::this_thread::sleep_for(std::chrono::milliseconds(COOKING_TIME_MS));
 
         //lock rack and put burger in it
         rackMutex.acquire();
+        std::this_thread::sleep_for(std::chrono::milliseconds(COOKING_TIME_MS));
         rack++;
         rackMutex.release();
 
-        std::cout << "Produced: Rack=" << rack << std::endl;
-        cashierSemaphore.release(); //increment cashier sem
+        std::osyncstream(std::cout) << "[COOK " << id << "] " << "Putting burger onto rack" << "\n";
+        fullCount.release(); //increment cashier sem
     }
 }
 
-[[noreturn]] void cashier()
+[[noreturn]] void cashier(int id)
 {
     //for debugging, cashier takes a burger out every 2s
     //in actual program replace with only when the customer wants
     while(true)
     {
-        sleep(3);
-        cashierSemaphore.acquire(); //decrement cashier sem
+        customerSem.acquire(); //wait for customers
+        fullCount.acquire();
 
         //lock rack and take burger out
         rackMutex.acquire();
         rack--;
         rackMutex.release();
 
-        std::cout << "Consumed: Rack=" << rack << std::endl;
-        cookSemaphore.release(); //increment cook sem
+        std::osyncstream(std::cout) << "[CASHIER " << id << "] " << "Taking burger from rack" << "\n";
+        emptyCount.release();
+        burger.release(); //signal that burger is given
     }
 }
 
+void customer(int id)
+{
+    customerMutex.acquire(); //enter CS for one customer at a time
+    sleep(2);
+    //CS
+    //signal cashier that a customer is present
+    customerSem.release();
+    burger.acquire();
+    std::osyncstream(std::cout) << "[CUSTOMER " << id << "] " << "Received burger" << "\n";
+    customerMutex.release(); //leave CS, allow other customers to enter CS and request for burgers
+
+}
 
 
 int main()
 {
-    std::thread cook_thread(cook);
-    std::thread cashier_thread1(cashier);
-    std::thread cashier_thread2(cashier);
+    //Check if config file exists
+    std::ifstream myFile;
+    myFile.open("config.ini");
+    if(!myFile)
+    {
+        std::cout << "Config file does not exist!" << std::endl;
+        return 0; //if it does not exist then exit
+    }
+    //otherwise load the config.ini file
+    ini::IniFile configFile;
+    configFile.decode(myFile);
+    NUMBER_OF_COOKS = configFile["config"]["cooks"].as<int>();
+    NUMBER_OF_CASHIERS = configFile["config"]["cashiers"].as<int>();
+    NUMBER_OF_CUSTOMERS = configFile["config"]["customers"].as<int>();
+    COOKING_TIME_MS = configFile["config"]["cooking_time_ms"].as<int>();
+    RACK_SIZE = configFile["config"]["rack"].as<int>();
 
 
-    cook_thread.join();
-    cashier_thread1.join();
-    cashier_thread2.join();
+    std::cout << "=====Burger Buddies Problem=====" << std::endl;
 
-    std::cout << "Burger Buddies Problem" << std::endl;
+    std::vector<std::thread> cooksArr;
+    std::vector<std::thread> cashiersArr;
+    std::vector<std::thread> customersArr;
+
+
+    //Starting all cooks
+    for(int i = 0; i < NUMBER_OF_COOKS; i++)
+    {
+        cooksArr.emplace_back(cook, i);
+    }
+    //Starting all cashiers
+    for(int i = 0; i < NUMBER_OF_CASHIERS; i++)
+    {
+        cashiersArr.emplace_back(cashier, i);
+    }
+    //Starting all customers
+    for(int i = 0; i < NUMBER_OF_CUSTOMERS; i++)
+    {
+        customersArr.emplace_back(customer, i);
+    }
+
+
+
+    for(auto& x : cooksArr)
+    {
+        x.join();
+    }
+    for(auto& x : cashiersArr)
+    {
+        x.join();
+    }
+    for(auto& x : customersArr)
+    {
+        x.join();
+    }
+
     return 0;
 }
